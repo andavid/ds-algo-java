3ec1047e3a401fb6fa124af8940b03a6ebc68ae2
andavid <jiankedai@gmail.com>
2019-04-15 23:30:50 +0800

    add usage of stack and queue

---
+++ src/main/java/com/github/andavid/ds/basic/queue/QueueUsage.java
@@ -0,0 +1,26 @@
+package com.github.andavid.ds.basic.queue;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class QueueUsage {
+  public static void main(String[] args) {
+    // 1. Initialize a queue.
+    Queue<Integer> q = new LinkedList<>();
+    // 2. Get the first element - return null if queue is empty.
+    System.out.println("The first element is: " + q.peek());
+    // 3. Push new element.
+    q.offer(5);
+    q.offer(13);
+    q.offer(8);
+    q.offer(6);
+    // 4. Pop an element.
+    q.poll();
+    // 5. Get the first element.
+    System.out.println("The first element is: " + q.peek());
+    // 7. Get the size of the queue.
+    System.out.println("The size is: " + q.size());
+    // 8. Check if queue is empty.
+    System.out.println("The queue is empty: " + q.isEmpty());
+  }
+}
