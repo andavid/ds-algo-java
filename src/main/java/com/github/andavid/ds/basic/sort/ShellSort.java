b3d1ece559dcddd2faa7376db003bb814be7bd2f
andavid <jiankedai@gmail.com>
2019-06-12 11:30:32 +0800

    add shell sort

---
+++ src/main/java/com/github/andavid/ds/basic/sort/ShellSort.java
@@ -0,0 +1,27 @@
+package com.github.andavid.ds.basic.sort;
+
+public class ShellSort {
+
+  public void sort(int[] array) {
+    if (array == null || array.length <= 1) {
+      return;
+    }
+    shellSort(array, array.length);
+  }
+
+  public void shellSort(int[] array, int n) {
+    int gap = n / 2;
+    while (gap >= 1) {
+      for (int i = gap; i < n; i++) {
+        int temp = array[i];
+        int j = i - gap;
+        while (j >= 0 && array[j] > temp) {
+          array[j + gap] = array[j];
+          j = j - gap;
+        }
+        array[j + gap] = temp;
+      }
+      gap = gap / 2;
+    }
+  }
+}
