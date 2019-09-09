245194f084b444ccf7458f54e1d93c80ac7959a4
andavid <jiankedai@gmail.com>
2019-09-09 17:12:23 +0800

    modify skip list test data

--- src/test/java/com/github/andavid/ds/datastructure/skiplist/SkipListTest.java
+++ src/test/java/com/github/andavid/ds/datastructure/skiplist/SkipListTest.java
@@ -8,25 +8,22 @@ public class SkipListTest {
   @Test
   public void testSkipList() {
     SkipList skipList = new SkipList();
-    skipList.insert(1);
-    skipList.insert(3);
-    skipList.insert(5);
-    skipList.insert(7);
-    skipList.insert(9);
-    skipList.insert(12);
+    for (int i = 1; i <= 32; i++) {
+      skipList.insert(i);
+    }
     System.out.println(skipList);
 
-    Node node = skipList.find(5);
+    Node node = skipList.find(11);
     if (node != null) {
       System.out.println(node);
     } else {
       System.out.println("not found");
     }
 
-    skipList.delete(5);
+    skipList.delete(11);
     System.out.println(skipList);
 
-    node = skipList.find(5);
+    node = skipList.find(11);
     if (node != null) {
       System.out.println(node);
     } else {
