aacde58eaa06c66ea1cebb8aaef54a0781e4a91e
andavid <jiankedai@gmail.com>
2019-11-12 17:34:13 +0800

    modify brute force

--- src/main/java/com/github/andavid/ds/algorithm/stringmatch/BruteForce.java
+++ src/main/java/com/github/andavid/ds/algorithm/stringmatch/BruteForce.java
@@ -4,6 +4,32 @@ package com.github.andavid.ds.algorithm.stringmatch;
  * 字符串朴素匹配算法
  */
 public class BruteForce {
+
+  public int match(String txt, String pattern) {
+    if (txt == null || pattern == null || txt.length() < pattern.length()) {
+      return -1;
+    }
+
+    int i = 0;
+    int j = 0;
+    while (i < txt.length() && j < pattern.length()) {
+      if (txt.charAt(i) == pattern.charAt(j)) {
+        // 当前字符匹配成功，主串和模式串都往后移动一位
+        i++;
+        j++;
+      } else {
+        // 当前字符匹配失败，主串回溯到匹配开始位置(i-j)的后面一位(i-j+1)，模式串从头开始匹配
+        i = i - j + 1;
+        j = 0;
+      }
+    }
+
+    if (j == pattern.length()) {
+      return i - j;
+    }
+    return -1;
+  }
+
   public int find(String txt, String pattern) {
     if (txt == null || pattern == null || txt.length() < pattern.length()) {
       return -1;
--- src/test/java/com/github/andavid/ds/algorithm/stringmatch/BruteForceTest.java
+++ src/test/java/com/github/andavid/ds/algorithm/stringmatch/BruteForceTest.java
@@ -7,6 +7,10 @@ public class BruteForceTest {
   @Test
   public void testBruteForce() {
     BruteForce bf = new BruteForce();
+    System.out.println(bf.match("abcdef", "def"));
+    System.out.println(bf.match("aaaaab", "ab"));
+    System.out.println(bf.match("aaaaaa", "ab"));
+    System.out.println();
     System.out.println(bf.find("abcdef", "def"));
     System.out.println(bf.find("aaaaab", "ab"));
     System.out.println(bf.find("aaaaaa", "ab"));
